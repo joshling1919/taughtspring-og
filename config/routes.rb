12efe0e1b2e1c21cb779d20dc0c6dac355700c21
Joshua <joshling1919@gmail.com>
2016-08-30 00:14:41 -0700

    set up the root main

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "static_pages#root"
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy]
