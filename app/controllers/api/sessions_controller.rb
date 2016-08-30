0230981a07b292564101464f336d1895930b0b0f
Joshua <joshling1919@gmail.com>
2016-08-29 23:30:16 -0700

    added users and sessions controllers

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,28 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if @user
+      log_in(@user)
+      render "api/users/show"
+    else
+      render json: ["Invalid email and password combination"], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+
+    if @user
+      log_out
+      render "api/users/show"
+    else
+      render json: ["Nobody's signed in!"], status: 404
+    end
+  end
+
 end
