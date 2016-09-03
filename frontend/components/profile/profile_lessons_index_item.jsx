9d5cffcdd56ad8d3970939795df3de94357bf5f8
Joshua <joshling1919@gmail.com>
2016-09-02 17:20:57 -0700

    added links to lesson show page on the profile index items

--- frontend/components/profile/profile_lessons_index_item.jsx
+++ frontend/components/profile/profile_lessons_index_item.jsx
@@ -5,6 +5,7 @@ const ProfileLessonsIndexItem = ({ lesson }) => {
   return(
     <div className="profile-item-container">
       <li className="profile-lessons-item">
+        <a href={`#/lessons/${lesson.id}`}>{ lesson.title }</a>
         <img src={ lesson.thumbnail_url } />
       </li>
     </div>
