8b7a5869c9d9d35931a60a8823c6b2ca0515c63c
Joshua <joshling1919@gmail.com>
2016-09-03 19:59:39 -0700

    styled nav bar

--- frontend/components/lessons/lessons_index_item.jsx
+++ frontend/components/lessons/lessons_index_item.jsx
@@ -4,15 +4,18 @@ const LessonsIndexItem = ({ lesson }) => {
   const { title, id, user, image_url } = lesson;
   return(
     <li className="index-item">
-      <a className="lessonTitle" href={`#lessons/${id}`}>{title}</a>
+      <div className="title-container">
+        <a className="title" href={`#lessons/${id}`}>{title}</a>
+      </div>
       <div className="index-pic-background">
         <div className="index-pic-container">
           <img className="index-pic" src={image_url}/>
         </div>
       </div>
-      <span className="author">
-      by: <a href={`#/profiles/${user.id}`}>{user.first_name} {user.last_name}</a>
-      </span>
+      <div className="index-description">
+        by: <a
+        href={`#/profiles/${user.id}`}>{user.first_name} {user.last_name}</a>
+      </div>
     </li>
   );
 };
--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -70,28 +70,33 @@ class NavBar extends React.Component{
   render(){
     return(
       <ul className="nav-bar">
-        <li className="logo" id='nav-bar-item' onClick={this._goHome.bind(this)}>
-          Taught Spring
+        <li className="logo-container" onClick={this._goHome.bind(this)}>
+          <img className="logo"
+            src="https://lh3.googleusercontent.com/Wt_SNZ5ao7hCOUjdkFzDlwzqZlO7bzEWv3dLdCuB1PWP6Hv3mbRpyAoPLL8FzK-nzfT7Qr-Ezx0QQoTnATX9uAcPfzhW-7oO_VH0nTcCKEuJnsiRRPdhuLgCbl5y-OgEhCSrtAhYqnlfQ_lEJNFeihIy7sRhTQzi_GAB7ve2mRSNGoiAxLHlLOpaUetlwRq0F9q0cn8YdXBi6bhUqvciJtPy-a_4Z9ePKUW3JQYYkuTQKm0E70f_e-_qEIFAcUgjNphGMCRtZKMzCYads6LFXhpomqfGU1MZO-nN9-CdpaN8SAZT11NzYzxBJywrXuck_ZX2GQ5Uhydgc1m7AHre4hW6BPz_oKQGh-fIxewvOP_30J_R2sAi2_f-Bd4krgVKBJKOxv3Z6pzajt6rER4NT-IxWpw-EiezspC2ZP_HWrF3FDuQcQY4yL9ne7L7ZUQcl_uE_i3Cx1TGmQ2zCiVB97K_Vwcb7u7Z3N2hEg86u8JgKWBah6CCpF1OlXieE6dHotqykX3LBEM53P6jgw7Y0stG-otGvIOKumzuTcOvM81OntBApr84WSnIGw1dUzpZlRABZG0F9ABRsiDgk7lvt9rvnrxTVSMxZIiNSXAxFYORMDNc=w320-h170-no"
+            />
         </li>
-        <li id='nav-bar-item'>
-          <input className="search" type="search" placeholder="search..."></input>
+        <li className='nav-bar-search'>
+          <input className="search" type="search" placeholder="&#xf002;"></input>
         </li>
-        <li id='nav-bar-item'>
-          <button className="nav-button pure-button">Subjects</button>
-        </li>
-        <div>
-          <li id='nav-bar-item'>
-            <button
-              className="nav-button pure-button"
-              onClick={this._handleUser.bind(this)}
-              >{`${this._userButtonText()}`}</button>
-            {this._userDropdown()}
+        <div className="nav-buttons">
+          <li className='nav-bar-item'>
+            <button className="nav-button subject-button">Subjects</button>
+          </li>
+
+          <li className='nav-bar-item'>
+              <button
+                className="nav-button user-button"
+                onClick={this._handleUser.bind(this)}
+                >{`${this._userButtonText()}`}</button>
+              {this._userDropdown()}
+          </li>
+
+          <li className='nav-bar-item'>
+            <button onClick={this._handleLog.bind(this)}
+              className="nav-button log-button"
+              >{`${this._logButtonText()}`}</button>
           </li>
         </div>
-        <li id='nav-bar-item'>
-          <button onClick={this._handleLog.bind(this)}
-            className="nav-button pure-button">{`${this._logButtonText()}`}</button>
-        </li>
       </ul>
 
     );
